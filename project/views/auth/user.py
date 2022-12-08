7b9e3d324a5a6db4ba960a0dbe17a0cc57b826cb
Denis <molatok@ya.ru>
2022-12-09 00:49:39 +0300

    удалил лишние импорты

--- project/views/auth/user.py
+++ project/views/auth/user.py
@@ -1,8 +1,7 @@
-from flask import request, jsonify
+from flask import request
 from flask_restx import Namespace, Resource
 from project.container import user_service
 from project.setup.api.models import user
-from project.services.users_service import UsersService
 from project.setup.api.parsers import page_parser
 from project.tools.decorators import auth_required
 from project.tools.security import user_token_by_email
@@ -18,7 +17,6 @@ class RegisterView(Resource):
         return user_service.get_all(**page_parser.parse_args())
     
     @auth_required
-    # @api.marshal_with(user, as_list=True, code=200, description='OK')
     def patch(self):
         token = request.headers["Authorization"].split("Bearer ")[-1]
         user_up = request.json
